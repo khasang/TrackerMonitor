38f0c43fff1b26f091031ab13ac5ef2c7cbaa39e
Rammun <russslllan@mail.ru>
2015-10-16 08:04:27 +0300

    Добавил DBInitializer

---
+++ DAL/DBInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DBInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        private Random rnd = new Random();
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            // Здесь инициализируем БД
+        }
+    }
+}
---
+++ DAL/Entities/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Здесь добавьте утверждения пользователя
+            return userIdentity;
+        }
+    }
+}
