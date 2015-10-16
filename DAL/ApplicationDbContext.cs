7035a602734a14ecd4c3dae7b24c8e26d958f954
Rammun <russslllan@mail.ru>
2015-10-16 08:32:15 +0300

    Добавил классы для настроек сущностей EF

--- DAL/ApplicationDbContext.cs
+++ DAL/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace DAL
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        //      Здесь подключаем сущности EF
+        // public DbSet<Entity> Entities { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -19,5 +23,17 @@ namespace DAL
         {
             return new ApplicationDbContext();
         }
+
+        /*
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Здесь подключаем настройки связей сущностей между собой
+
+            modelBuilder.Configurations.Add(new ClassMapper());
+         
+
+            base.OnModelCreating(modelBuilder);
+        }
+         */
     }
 }
---
+++ DAL/EntityConfigurations/ClassMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EntityConfigurations
+{
+    // Здесь настраиваем связи сущностей EF
+
+    //public class ClassMapper : EntityTypeConfiguration<Article>
+    //{
+    //    public ClassMapper()
+    //    {
+    //        this.HasMany(a => a.Comments)
+    //            .WithRequired(c => c.Article)
+    //            .HasForeignKey(c => c.ArticleId)
+    //            .WillCascadeOnDelete(true);
+    //    }
+    //}
+}
