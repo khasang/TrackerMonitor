75310ea7e9bd0849749645f881a47379390688c7
qdimka <qdimka1991@mail.ru>
2015-10-28 21:54:09 +0300

    Получилось отправлять конкретному пользователю

--- WebMVC/Hubs/PushNotify.cs
+++ WebMVC/Hubs/PushNotify.cs
@@ -9,31 +9,34 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace WebMVC.Hubs
 {
     /// <summary>
     /// Класс для работы с SignalR
     /// </summary>
+    //[Authorize]
     public class PushNotify:Hub
     {
-        UserManager<ApplicationUser> _userManager;
-        ApplicationDbContext _context = new ApplicationDbContext();
-
-        public PushNotify()
-        {
-            _userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
-        }
-
+        [Authorize]
         public void SendNewMessage(GPSTrackerMessage message)
         {
-            //if (message.GPSTracker.Owner == _userManager.FindById(Context.User.Identity.GetUserId()))
-            //{
+            Debug.WriteLine(Context.User.Identity.Name);
+            Clients.Group("admin@admin.com").ShowMessage(message); 
+            //у определенного пользователя группа в честь его username, которая содержит все его ConnectionId
+            Clients.Group("admin@ad.com").ShowMessage(message);
+        }
 
-            //Cогласно докам по сигнал р используется IPrincipal.Identity.Name as the user name, но можно изменить
-            Clients.User("3277c404-708b-43b8-9bbd-e6e65a869d3f").ShowMessage(message); //айди админа
-            //}
-            //Clients.All.ShowMessage(message);
+        /// <summary>
+        /// Коннект нового пользователя, добавление новой группы с ConnectionId
+        /// </summary>
+        /// <returns></returns>
+        public override Task OnConnected()
+        {
+            Debug.WriteLine(Context.ConnectionId);
+            Groups.Add(Context.ConnectionId, Context.User.Identity.Name); //имя группы - имя пользователя
+            return base.OnConnected();
         }
 
     }
