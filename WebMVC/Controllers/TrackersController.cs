cc434d42ecf419dced9c71b9127f39cf32a1e156
marcheneli <marcheneli@gmail.com>
2015-11-11 13:55:26 +0300

    add tracker edit view

--- WebMVC/Controllers/TrackersController.cs
+++ WebMVC/Controllers/TrackersController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using DAL.Entities;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WebMVC.Models.TrackersViewModels;
@@ -17,18 +20,65 @@ namespace WebMVC.Controllers
 
         public ActionResult Register()
         {
-            return View();
+            RegisterViewModel model = new RegisterViewModel();
+
+            return View(model);
         }
 
         [HttpPost]
         public ActionResult Register(RegisterViewModel model)
         {
-            return View();
+            if (!ModelState.IsValid)
+                return View(model);
+
+            GPSTracker tracker = new GPSTracker();
+            tracker.Name = model.Name;
+            tracker.OwnerId = User.Identity.GetUserId();
+
+            dbContext.GPSTrackers.Add(tracker);
+
+            return RedirectToAction("Index");
         }
 
-        public ActionResult Edit()
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if(id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            GPSTracker tracker = dbContext.GPSTrackers.Find(id);
+
+            if (tracker == null)
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+            if (tracker.OwnerId != User.Identity.GetUserId())
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+
+            EditViewModel model = new EditViewModel();
+
+            model.Id = tracker.Id;
+            model.Name = tracker.Name;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            GPSTracker tracker = dbContext.GPSTrackers.Find(model.Id);
+
+            if(tracker == null)
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+            if(tracker.OwnerId != User.Identity.GetUserId())
+                return new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+
+            tracker.Name = model.Name;
+            dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
 
         public ActionResult Map()
---
+++ WebMVC/Models/TrackersViewModels/EditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models.TrackersViewModels
+{
+    public class EditViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+    }
+}
