d010365db83eee516fb914d7e64622ef88e04aa1
marcheneli <marcheneli@gmail.com>
2015-11-11 10:16:55 +0300

    add trackers controller

---
+++ WebMVC/Controllers/TrackersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class TrackersController : BaseController
+    {
+        // GET: Trackers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            return View();
+        }
+
+        public ActionResult ConfirmDelete()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
