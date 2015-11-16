e71b69595668af50ea9358ea64ab179a8b1a4e0a
marcheneli <marcheneli@gmail.com>
2015-11-16 23:18:54 +0300

    added gps points list

--- TestMessageSender/Program.cs
+++ TestMessageSender/Program.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using DAL.Entities;
+using System.Globalization;
 
 namespace TestMessageSender
 {
@@ -15,6 +16,9 @@ namespace TestMessageSender
     {
         static void Main(string[] args)
         {
+            //id двух пользователей из бд
+
+
             HubConnection hubConnection = new HubConnection(@"http://localhost:3254");
             IHubProxy hubProxy = hubConnection.CreateHubProxy("PushNotify");
             hubConnection.Start().Wait();
@@ -22,23 +26,49 @@ namespace TestMessageSender
 
             Random r = new Random();
             Console.WriteLine("Service started...");
-            while (true)
+            foreach (var message in GetGPSMessages())
             {
-                GPSTrackerMessage message = new GPSTrackerMessage()
-                {
-                    Id = r.Next(2),
-                    GPSTrackerId = r.Next(30).ToString(),
-                    Latitude = 31 + r.Next(20),
-                    Longitude = 31 - r.Next(20),
-                    Time = DateTime.Now,
-                    
-                };
-                //вызов метода из хаба PushNotify
+                //вызов метода из хаба PushNotify и передача ему сообщения.
                 hubProxy.Invoke("SendNewMessage", message);
-                Console.WriteLine("...Send {0} - {1} - {2} - {3} - {4} ..... ok", message.Id, message.GPSTrackerId, message.Latitude, message.Longitude, message.Time);
+                Console.WriteLine($"...Send {message.GPSTracker.OwnerId} - {message.GPSTrackerId} - {message.Latitude} - {message.Longitude} - {message.Time} ..... ok");
                 Console.WriteLine();
                 Thread.Sleep(new Random().Next(1000, 4000));
             }
+        }
+
+        private static IEnumerable<GPSTrackerMessage> GetGPSMessages()
+        {
+            Random r = new Random();
+            List<GPSTrackerMessage> messages = new List<GPSTrackerMessage>();
+
+            string gpsPointsFilePath = @"C:\Users\Николай\Documents\Visual Studio 2015\Projects\GPSTracker\GPSPoints\GPSPoints.csv";
+            string[] guids = new string[] { "3277c404-708b-43b8-9bbd-e6e65a869d3f", "a49b8728-6d68-4029-a3a9-a1581edb2d7e" };
+
+            using (StreamReader sr = new StreamReader(gpsPointsFilePath))
+            {
+                CultureInfo usCulture = new CultureInfo("en-US");
+                NumberFormatInfo dbNumberFormat = usCulture.NumberFormat;
+
+                while (!sr.EndOfStream)
+                {
+                    string[] coordinates = sr.ReadLine().Split(';');
+
+                    messages.Add(new GPSTrackerMessage
+                    {
+                        Id = r.Next(2),
+                        GPSTrackerId = r.Next(30).ToString(),
+                        Latitude = double.Parse(coordinates[0], dbNumberFormat),
+                        Longitude = double.Parse(coordinates[1], dbNumberFormat),
+                        GPSTracker = new GPSTracker()
+                        {
+                            OwnerId = guids[r.Next(2)]
+                        },
+                        Time = DateTime.Now
+                    });
+                }
+            }
+
+            return messages;
         }
     }
 }
