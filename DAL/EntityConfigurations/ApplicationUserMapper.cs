9e2a281bf071aaa68a622cd59f88ee96edb61ea1
marcheneli <marcheneli@gmail.com>
2015-11-18 20:27:41 +0300

    add application user mapper

---
+++ DAL/EntityConfigurations/ApplicationUserMapper.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EntityConfigurations
+{
+    public class ApplicationUserMapper : EntityTypeConfiguration<ApplicationUser>
+    {
+        public ApplicationUserMapper()
+        {
+            this.HasOptional(u => u.UserProfile)
+                .WithRequired(p => p.User);
+        }
+    }
+}
