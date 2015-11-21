2612c3f1bf98c074e12ea1e96a44f7fcfc59baf1
marcheneli <marcheneli@gmail.com>
2015-11-21 14:57:20 +0300

    added EditViewModel to PersonalArea

---
+++ WebMVC/Models/PersonalAreaViewModels/EditViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMVC.Models.PersonalAreaViewModels
+{
+    public class EditViewModel
+    {
+        public string Name { get; set; }
+    }
+}
