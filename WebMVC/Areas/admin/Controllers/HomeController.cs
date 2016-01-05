13297429f88c003d7486fe857d36f2455654faf3
qdimka <qdimka1991@mail.ru>
2016-01-05 09:25:26 +0300

    add namespace in RouteConfig.cs

---
+++ WebMVC/Areas/admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Areas.admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
