8761fce1425b3214c4ff2bfea9d977213b4714ca
qdimka <qdimka1991@mail.ru>
2016-01-05 11:13:52 +0300

    add Controllers in Admin panel

---
+++ WebMVC/Areas/admin/Controllers/GpsTrackersController.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using DAL.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Areas.admin.Controllers
+{
+    public class GpsTrackersController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: admin/GpsTrackers
+        public async Task<ActionResult> Index()
+        {
+            var gpsTrackers = db.GPSTrackers;
+            if (gpsTrackers == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(await gpsTrackers.ToListAsync());
+        }
+    }
+}
---
+++ WebMVC/Areas/admin/Controllers/UserProfilesController.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using DAL.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Areas.admin.Controllers
+{
+    public class UserProfilesController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+        UserManager<ApplicationUser> userManager;
+
+        public UserProfilesController()
+        {
+            userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+        }
+        // GET: admin/UserProfiles
+        /// <summary>
+        /// Возвратит нам всех профайлы пользователей.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> Index()
+        {
+            var userProfiles = db.UserProfiles;
+            if (userProfiles == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(await userProfiles.ToListAsync());
+        }
+    }
+}
