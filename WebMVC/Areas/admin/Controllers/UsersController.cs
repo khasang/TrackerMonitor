d14fb131a245258c8315d29907963b4fa8585aad
qdimka <qdimka1991@mail.ru>
2016-01-09 02:07:18 +0300

    Единое представление для создания просмотра и редактирования, дописать контроллеры

--- WebMVC/Areas/admin/Controllers/UsersController.cs
+++ WebMVC/Areas/admin/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -51,13 +52,82 @@ namespace WebMVC.Areas.admin.Controllers
                     UserName = model.UserName,
                     Email = model.Email,
                     EmailConfirmed = model.EmailConfirmed,
-                    PhoneNumber = model.PhoneNumber
+                    PhoneNumber = model.PhoneNumber,
+                    PhoneNumberConfirmed = model.PhoneNumberConfirmed,
+                    AccessFailedCount = model.AccessFailedCount,
+                    //Claims = model.Claims, только чтение
+                    LockoutEnabled = model.LockoutEnabled,
+                    LockoutEndDateUtc = model.LockoutEndDateUtc,
+                    //Logins = model.Logins, только чтение
+                    PasswordHash = model.PasswordHash,
+                    //Roles = model.Roles, только чтение
+                    SecurityStamp = model.SecurityStamp,
+                    TwoFactorEnabled = model.TwoFactorEnabled,
+                    UserProfile = model.UserProfile,
+                    UserProfileId = model.UserProfileId
                 };
                 db.Users.Add(user);
                 db.SaveChanges();
                 return Json(new { success = true });
             }
             return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        //[HttpGet]
+        //public async Task<ActionResult> Details(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    var user = await db.Users.FirstAsync(i => i.Id == id);
+        //    if (user == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return PartialView("Details",user);
+        //}
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = await db.Users.FirstAsync(i => i.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("Edit", user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var modifyUser = db.Users.Find(user.Id);
+                modifyUser.Id = user.Id;
+                modifyUser.UserName = user.UserName;
+                modifyUser.Email = user.Email;
+                modifyUser.EmailConfirmed = user.EmailConfirmed;
+                modifyUser.PhoneNumber = user.PhoneNumber;
+                modifyUser.PhoneNumberConfirmed = user.PhoneNumberConfirmed;
+                modifyUser.AccessFailedCount = user.AccessFailedCount;
+                modifyUser.LockoutEnabled = user.LockoutEnabled;
+                modifyUser.LockoutEndDateUtc = user.LockoutEndDateUtc;
+                modifyUser.PasswordHash = user.PasswordHash;
+                modifyUser.SecurityStamp = user.SecurityStamp;
+                modifyUser.TwoFactorEnabled = user.TwoFactorEnabled;
+                modifyUser.UserProfile = user.UserProfile;
+                modifyUser.UserProfileId = user.UserProfileId;
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return PartialView("Edit",user);
         }
     }
 }
