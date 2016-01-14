848669cf936258463f8ba3b80f53e3c384454207
qdimka <qdimka1991@mail.ru>
2016-01-14 23:05:29 +0400

    add messages

---
+++ WebMVC/Areas/admin/Controllers/GpsTrackerMessagesController.cs
@@ -0,0 +1,131 @@
+﻿using DAL;
+using DAL.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Areas.admin.Controllers
+{
+    public class GpsTrackerMessagesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        UserManager<ApplicationUser> userManager;
+
+        public GpsTrackerMessagesController()
+        {
+            userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+        }
+
+        // GET: admin/GpsTrackersMessages
+        public async Task<ActionResult> Index()
+        {
+            var gpsTrackersMessages = db.GPSTrackerMessages;
+            if (gpsTrackersMessages == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(await gpsTrackersMessages.ToListAsync());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var gpsTrackersMessage = new GPSTrackerMessage();
+            ViewBag.Selector = ViewSelector.Edit;
+            ViewBag.ActionName = "Добавить сообщение";
+            return View("Dialog", gpsTrackersMessage);
+        }
+
+        [HttpPost]
+        public ActionResult Create(GPSTrackerMessage model)
+        {
+            ViewBag.Selector = ViewSelector.Edit;
+            ViewBag.ActionName = "Добавить сообщение";
+            if (ModelState.IsValid)
+            {
+                var gpsTrackersMessage = new GPSTrackerMessage()
+                {
+                    Id = model.Id,
+                    Latitude = model.Latitude,
+                    Longitude = model.Longitude,
+                    Time = model.Time,
+                    GPSTrackerId = model.GPSTrackerId
+                };
+                db.GPSTrackerMessages.Add(gpsTrackersMessage);
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return View("Dialog", model);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var gpsTrackersMessage = await db.GPSTrackerMessages.FirstAsync(i => i.Id == id);
+            ViewBag.Selector = ViewSelector.Edit;
+            ViewBag.ActionName = "Редактировать";
+            if (gpsTrackersMessage == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("Dialog", gpsTrackersMessage);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(GPSTrackerMessage gpsTrackersMessage)
+        {
+            ViewBag.Selector = ViewSelector.Edit;
+            ViewBag.ActionName = "Редактировать";
+            if (ModelState.IsValid)
+            {
+                var modifyMessage = db.GPSTrackerMessages.Find(gpsTrackersMessage.Id);
+                modifyMessage.Id = gpsTrackersMessage.Id;
+                modifyMessage.Latitude = gpsTrackersMessage.Latitude;
+                modifyMessage.Longitude = gpsTrackersMessage.Longitude;
+                modifyMessage.Time = gpsTrackersMessage.Time;
+                modifyMessage.GPSTrackerId = gpsTrackersMessage.GPSTrackerId;
+                db.SaveChanges();
+                return Json(new { success = true });
+            }
+            return PartialView("Dialog", gpsTrackersMessage);
+        }
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var gpsTrackersMessage = await db.GPSTrackerMessages.FirstAsync(i => i.Id == id);
+            ViewBag.Selector = ViewSelector.Delete;
+            ViewBag.ActionName = "Удалить";
+            if (gpsTrackersMessage == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("Dialog", gpsTrackersMessage);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirm(int? id)
+        {
+            var gpsTrackersMessage = await db.GPSTrackerMessages.FirstAsync(i => i.Id == id);
+            db.GPSTrackerMessages.Remove(gpsTrackersMessage);
+            db.SaveChanges();
+            return Json(new { success = true });
+        }
+    }
+}
