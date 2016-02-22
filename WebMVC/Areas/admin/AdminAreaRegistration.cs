6a0d11553efca5e94d2cb479ac44c5c5456e69e1
marcheneli <marcheneli@gmail.com>
2016-02-22 22:47:53 +0300

    Add adminAreaRegistration

---
+++ WebMVC/Areas/admin/AdminAreaRegistration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVC.Areas.admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "admin_default",
+                "admin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
